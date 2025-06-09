c4af43170293aa657e8423ca698b6d56823380c0
GHIVARI MGP <fahripardiansyah@gmail.com>
2025-06-09 20:46:43 +0700

    export data dosen alhamdulillah amannnn

--- app/Http/Controllers/Services/Convert/ExportController.php
+++ app/Http/Controllers/Services/Convert/ExportController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Services\Convert;
 
+use App\Exports\DosenExport;
 use App\Exports\StudentExport;
 use App\Exports\UsersExport;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DosenExportRequest;
 use App\Http\Requests\MahasiswaExportRequest;
 use Maatwebsite\Excel\Facades\Excel;
 use Rap2hpoutre\FastExcel\FastExcel;
@@ -35,6 +37,18 @@ class ExportController extends Controller
             'xls' => Excel::download(new StudentExport, $filename . '.xls', \Maatwebsite\Excel\Excel::XLS),
             'csv' => Excel::download(new StudentExport, $filename . '.csv', \Maatwebsite\Excel\Excel::CSV),
             'html' => Excel::download(new StudentExport, $filename . '.html', \Maatwebsite\Excel\Excel::HTML),
+        };
+    }
+
+    public function exportDosens(DosenExportRequest $request)
+    {
+        $filename = 'daftar-dosen-' . date('d-m-Y');
+
+        return match ($request->extension) {
+            'xlsx' => Excel::download(new DosenExport, $filename . '.xlsx', \Maatwebsite\Excel\Excel::XLSX),
+            'xls' => Excel::download(new DosenExport, $filename . '.xls', \Maatwebsite\Excel\Excel::XLS),
+            'csv' => Excel::download(new DosenExport, $filename . '.csv', \Maatwebsite\Excel\Excel::CSV),
+            'html' => Excel::download(new DosenExport, $filename . '.html', \Maatwebsite\Excel\Excel::HTML),
         };
     }
 }
